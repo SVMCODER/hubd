b77001c459c5f359e78bc3c24017abea46f69724
svmcoder <124701282+SVMCODER@users.noreply.github.com>
2023-02-20 23:58:22 +0530

    Update code.js

--- code.js
+++ code.js
@@ -18,31 +18,3 @@ document.querySelector('#input').innerHTML = `
 </html>
 <!-- BOILER PLATE ENDS -->
 `
-function save() {
-    pname = document.getElementById('pname').value;
-
-
-    pauthor = localStorage.getItem('name')
-    time = Date().replace('GMT+0530 (India Standard Time)','')
-  
-       
-
-
-        localStorage.setItem('projectfile',localStorage.getItem('projectfile')+`
-        <br>
-        <div align='center'>
-        <div class='pcnt button'>
-          <h5>${pname}</h5>
-          <img src='https://img.icons8.com/emoji/600/000000/file-folder-emoji.png' alt='NO INTERNET CONNECTION' class='pimg'>
-          <hr>
-          <h6 class='pp'>CONGRATULATIONS ON CREATING THIS PROJECT</h6>
-          <hr><br>
-          <p class='pp'>Coded on: ${time} </p>
-          <div>
-            <p class=-'pd'>Developer: ${pauthor} </p>
-          </div>
-        </div>
-        </div>`);
-        alert('SAVED!')
-    
-}
