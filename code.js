14c75f266bb599bd157ca7aca38d5d7a65f6cd10
svmcoder <124701282+SVMCODER@users.noreply.github.com>
2023-02-20 23:42:49 +0530

    Update code.js

--- code.js
+++ code.js
@@ -20,7 +20,7 @@ document.querySelector('#input').innerHTML = `
 `
 function save() {
     pname = document.getElementById('pname').value;
-    pcode = document.querySelector('#input').value;
+    pcode = document.getElementById('input').value;
     
     pauthor = localStorage.getItem('name')
     time = Date().replace('GMT+0530 (India Standard Time)','')
